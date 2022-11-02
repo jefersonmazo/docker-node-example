adcae9bd4f1d49fc7eac4c252b856e80405d8733
Juan Esteban Ruiz Mesa <juan.ruiz@arus.com.co>
2022-11-02 18:43:35 -0500

    actualizar docker file para kubernetes

--- app/routes/productosFerreteria.routes.js
+++ app/routes/productosFerreteria.routes.js
@@ -3,5 +3,11 @@ module.exports = (app) => {
   const router = require("express").Router();
   router.get("/productosFerreteria/consultar", productosFerreteria.findAll);
   router.post("/productosFerreteria/crear", productosFerreteria.create);
+  router.get("/test", function (req, res) {
+    res.send({
+        nodo: process.env.MY_NODE_NAME,
+        pod: process.env.MY_POD_NAME
+    });
+  });
   app.use("/api", router);
 };
